e4a2c336a9cded5daf8d12f6bf91143cff5766d9
sunyf <sunyf-c@glodon.com>
2018-07-27 10:05:41 +0800

    啊啊啊

--- assets/Script/start.js
+++ assets/Script/start.js
@@ -16,7 +16,7 @@ cc.Class({
     //开始游戏
     startGame: function(){
         cc.director.loadScene ('main',function(){
-            console.log('main is loaded11111111');
+            console.log('main is loaded');
         }) 
     },
     // called every frame
